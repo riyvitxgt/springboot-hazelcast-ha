185953ef9313800119aabb573088fac46424bbc9
zhukm <zhukm@hundsun.com>
2025-06-28 18:15:35 +0800

    常量提取

---
+++ src/main/java/com/zhukm/sync/Constants.java
@@ -0,0 +1,11 @@
+package com.zhukm.sync;
+
+public class Constants {
+    public static final String USER_CACHE_KEY = "userCache";
+    public static final String SESSION_CACHE_KEY = "sessionCache";
+    public static final String DEFAULT_CACHE_KEY = "default";
+
+    private Constants() {
+        // 私有化构造函数
+    }
+}
